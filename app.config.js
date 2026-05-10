4c067962d04a885c9aad25767b7a82359853f6d3
Гена <bragin.gena1998@gmail.com>
2026-05-10 10:46:58 +0500

    Fix dev client: remove gradleCommand, add app.config.js

---
+++ app.config.js
@@ -0,0 +1,68 @@
+import 'dotenv/config';
+
+const IS_DEV = process.env.APP_VARIANT === 'development';
+
+export default {
+  expo: {
+    name: IS_DEV ? 'M&M Dev' : 'Mind & Motion',
+    slug: 'mindandmotion-mobile',
+    version: '1.0.0',
+    orientation: 'portrait',
+    icon: './assets/icon.png',
+    userInterfaceStyle: 'light',
+    splash: {
+      image: './assets/splash-icon.png',
+      resizeMode: 'contain',
+      backgroundColor: '#000000',
+    },
+    plugins: [
+      [
+        'expo-build-properties',
+        {
+          android: {
+            usesCleartextTraffic: true,
+          },
+        },
+      ],
+      [
+        'expo-notifications',
+        {
+          icon: './assets/icon.png',
+          color: '#000000',
+          androidMode: 'default',
+          androidCollapsedTitle: IS_DEV ? 'M&M Dev' : 'Mind & Motion',
+        },
+      ],
+      'expo-secure-store',
+      'expo-localization',
+      'expo-sqlite',
+    ],
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: IS_DEV
+        ? 'com.bragin22.mindandmotionmobile.dev'
+        : 'com.bragin22.mindandmotionmobile',
+      infoPlist: {
+        NSFaceIDUsageDescription:
+          'Mind&Motion использует Face ID для входа в приложение.',
+      },
+    },
+    android: {
+      package: IS_DEV
+        ? 'com.bragin22.mindandmotionmobile.dev'
+        : 'com.bragin22.mindandmotionmobile',
+      adaptiveIcon: {
+        foregroundImage: './assets/adaptive-icon.png',
+        backgroundColor: '#000000',
+      },
+    },
+    web: {
+      favicon: './assets/favicon.png',
+    },
+    extra: {
+      eas: {
+        projectId: 'efa67276-4a28-4c36-94e4-73220f0e5bd0',
+      },
+    },
+  },
+};
